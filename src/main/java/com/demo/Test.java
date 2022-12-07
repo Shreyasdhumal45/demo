b873281922bf2a0d54513d441695731962b40879
unknown <shreyasdhumal4545@gmail.com>
2022-12-07 20:54:57 +0530

    added lines in test class

--- src/main/java/com/demo/Test.java
+++ src/main/java/com/demo/Test.java
@@ -8,9 +8,13 @@ import org.hibernate.cfg.Configuration;
 public class Test {
 public static void main(String[] args) {
 	Configuration cfg=new Configuration();
+	//add the cfg file in configure
+	
 	cfg.configure("hibernate.cfg.xml");
+	//build the session and open the session
 	SessionFactory sf=cfg.buildSessionFactory();
 	Session session =sf.openSession();
+	// begin the transaction
 	Transaction tr=session.beginTransaction();
 	
 	Info info=new Info();
